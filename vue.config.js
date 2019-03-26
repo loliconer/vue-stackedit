1a41cdbc8ec2240335c168b02704e34031f45b7f
lolicon <vuedev@163.com>
2019-03-26 10:50:47 +0800

    improve: update dependencies

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,10 @@
-const path = require('path')
-
 module.exports = {
-  lintOnSave: false,
   css: {
     extract: false,
     loaderOptions: {
       less: {
         strictMath: 'on'
       }
-    }
-  },
-  pluginOptions: {
-    'style-resources-loader': {
-      patterns: [
-        path.resolve(__dirname, 'src/less/Variables.less'),
-        path.resolve(__dirname, 'src/less/Mixins.less')
-      ],
-      preProcessor: 'less'
     }
   },
   chainWebpack: config => {
